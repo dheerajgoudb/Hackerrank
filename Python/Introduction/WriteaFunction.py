9d90dd337eb2aef1877a616758d6fd4624e1dfa6
Dheeraj Goud Borlla <dheerajgoud6@gmail.com>
2016-10-29 11:21:50 -0500

    function to find a leap year

---
+++ Python/Introduction/WriteaFunction.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+    leap = False
+    if 1900 <= year <= 10**5:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+        elif year % 4 == 0:
+            leap = True
+    return leap
